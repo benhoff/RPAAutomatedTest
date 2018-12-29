9f9254873dd295243a9d4b8baca6c8319c0e1975
Juan Restrepo <jcrestrepobedoya@gmail.com>
2018-12-28 22:47:05 -0500

    eliminadas rutas quemadas

--- testConfig.py
+++ testConfig.py
@@ -1,20 +1,13 @@
 import os
+import getpass
 
 config = {
     'BASE_DIR':os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     'RPA_TOOl':'UIPath',
-    'LOG_PATH':'C:\\Users\\ryzen2600x\\AppData\\Local\\UiPath\\Logs\\',
-    'RPA_TASK_FOLDER':'C:\\jenkins\\workspace\\RPAAutomatedTest_master\\',
-    'TEST_RESULTS_FOLDER': ['C:\\jenkins\\workspace\\RPAAutomatedTest_master\\giftList\\']
+    'LOG_PATH':f'C:\\Users\\{getpass.getuser()}\\AppData\\Local\\UiPath\\Logs\\',
+    'RPA_TASK_FOLDER':f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}\\RPAAutomatedTest\\',
+    'TEST_RESULTS_FOLDER': ['giftList\\']
 }
-
-# config = {
-#     'BASE_DIR':os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-#     'RPA_TOOl':'UIPath',
-#     'LOG_PATH':'C:\\Users\\juan.restrepo\\AppData\\Local\\UiPath\\Logs\\',
-#     'RPA_TASK_FOLDER':'C:\\Users\\juan.restrepo\\Desktop\\JC\\laboratory-solutions\\juan.restrepo\\Laboratory 2\\UI\\',
-#     'TEST_RESULTS_FOLDER': ['C:\\Users\\juan.restrepo\\Desktop\\JC\\laboratory-solutions\\juan.restrepo\\Laboratory 2\\UI\\giftList\\']
-# }
 
 if __name__=="__main__":
     pass
