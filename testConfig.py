795bdc57e07161b7c529f783048f13825b8e3f5e
juan restrepo <juan.restrepo@vivelabmed.org>
2019-01-02 11:56:05 -0500

    anadido '_dev'

--- testConfig.py
+++ testConfig.py
@@ -5,7 +5,7 @@ config = {
     'BASE_DIR':os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
     'RPA_TOOl':'UIPath',
     'LOG_PATH':f'C:\\Users\\{getpass.getuser()}\\AppData\\Local\\UiPath\\Logs\\',
-    'RPA_TASK_FOLDER':f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}\\RPAAutomatedTest\\',
+    'RPA_TASK_FOLDER':f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}\\RPAAutomatedTest_dev\\',
     'TEST_RESULTS_FOLDER': ['giftList\\']
 }
 
