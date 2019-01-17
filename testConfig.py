7f6b80db54cafc0d79f9eecb9d742072b480f48b
juan restrepo <juan.restrepo@vivelabmed.org>
2019-01-17 11:30:40 -0500

    merged office365LogginSteps with sendEmails Steps

--- testConfig.py
+++ testConfig.py
@@ -1,6 +1,8 @@
 import os
 import getpass
 
+## UIPATH GiftListBot Config
+
 # config = {
 #     'BASE_DIR':os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
 #     'RPA_TOOl':'UIPath',
@@ -8,6 +10,20 @@ import getpass
 #     'RPA_TASK_FOLDER':f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}\\',
 #     'TEST_RESULTS_FOLDER': ['']
 # }
+
+
+## AA 2.Office365Loggin Config
+
+# config = {
+#     'BASE_DIR':os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+#     'RPA_TOOl':'AutomationAnywhere',
+#     'AA_TASK_NAME':'2.Office365Loggin',
+#     'LOG_PATH':f'C:\\Users\\{getpass.getuser()}\\Documents\\Automation Anywhere Files\\Automation Anywhere\\My Tasks\\DAPI Template AA2\\N00XX\\1.O365Login\\Logs\\',
+#     'RPA_TASK_FOLDER':f'C:\\Users\\{getpass.getuser()}\\Documents\\Automation Anywhere Files\\Automation Anywhere\\My Tasks\\DAPI Template AA2\\N00XX\\1.O365Login\\',
+#     'TEST_RESULTS_FOLDER': [f'C:\\Users\\{getpass.getuser()}\\Documents\\Automation Anywhere Files\\Automation Anywhere\\My Tasks\\DAPI Template AA2\\N00XX\\1.O365Login\\Temps\\']
+# }
+
+## AA 3.sendEmails Config
 
 config = {
     'BASE_DIR':os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
