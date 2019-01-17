ebf3da3b9524f9c46980ff254b2db2656e857284
juan restrepo <juan.restrepo@vivelabmed.org>
2019-01-17 18:04:21 -0500

    added copyFile method to envinromentSteps

--- test/steps/environmentSteps.py
+++ test/steps/environmentSteps.py
@@ -1,5 +1,6 @@
 from utils.fileUtils import deleteFiles
 from utils.fileUtils import copyFiles
+from utils.fileUtils import copyFile
 from testConfig import config
 
 @given(u'All the residual files have been deleted')
@@ -14,4 +15,11 @@ def step_impl(context,testdata):
     copyFiles(
         srcFolder=f'{config["BASE_DIR"]}//RPAAutomatedTest//test//test_resources//{testdata}//', 
         dstFolder=f'{config["TEST_RESULTS_FOLDER"][0]}'
+    )
+
+@given(u'is {testdatafile} in the input folder named as {filename}')
+def step_impl(context,testdatafile,filename):
+    copyFile(
+        srcFile=f'{config["BASE_DIR"]}//RPAAutomatedTest//test//test_resources//testdata3//{testdatafile}', 
+        dstFile=f'{config["RPA_TASK_FOLDER"]}//Inputs//{filename}'
     )
